0ced364388c877f91e2333df06c8fc8bb9e95346
Arafat Patwary <farabipsk@gmail.com>
2024-06-30 15:13:33 +0600

    Using test code for projects

---
+++ week_2/Module_5/basicData.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    list<int> glint{12,34,45,56,41,7};
+    for (auto i  : glint)
+    {
+        cout<< i << ' ';
+    }
+    
+    // cin>> glint;
+
+
+
+    return 0;
+}
---
+++ week_3/testCode/constructor.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MyClass {  //Define The class
+    public:     //Access specifier
+    MyClass(){   //Constructor
+        cout<<"Hello Planet";
+    }
+};
+
+int main(){
+    MyClass myobj;   // Create an object of MyClass (this will call the constructor)
+
+    return 0;
+}
