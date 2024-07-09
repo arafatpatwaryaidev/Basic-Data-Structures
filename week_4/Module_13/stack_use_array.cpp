732cec209c3c7f2f88fa42526eb4d09bb73f11d7
Arafat Patwary <farabipsk@gmail.com>
2024-07-09 09:28:39 +0600

    Use stack

--- week_4/Module_13/stack_use_array.cpp
+++ week_4/Module_13/stack_use_array.cpp
@@ -1,43 +1,81 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class myStack
 {
-    public:
-        vector<int> v;
-        //operation of function
-        void push(int val)  // O(1)
-        {
-            v.push_back(val);
+public:
+    vector<int> v;
+    // operation of function
+    void push(int val) // O(1)
+    {
+        v.push_back(val);
+    }
 
-        }
-
-
-        void pop()   // O(1)
+    void pop() // O(1)
+    {
+        // value delete or remove
+        v.pop_back();
+    }
+    int top() // O(1)
+    {
+        // value access
+        return v.back();
+    }
+    int size() // O(1)
+    {
+        return v.size();
+    }
+    bool empty() // O(1)
+    {
+        if (v.size() == 0)
         {
-            //value delete or remove
-            v.pop_back();
+            return true;
         }
-        int top()   // O(1)
+        else
         {
-            //value access
-            return v.back();
+            return false;
         }
-        int size()   // O(1)
-        {
-            return v.size();
-        }
-        bool empty()   // O(1)
-        {
-            if(v.size() == 0){
-                return true;
-            } else{
-                return false;
-            }
-        }
+    }
 };
 
-int main(){
+int main()
+{
+
+    myStack st;
+    //if value less use this method
+    // st.push(10);
+    // st.push(20);
+    // st.push(30);
+    // st.push(40);
+
+    // cout << st.top() << endl;
+    // st.pop();
+    // cout << st.top() << endl;
+    // st.pop();
+    // cout << st.top() << endl;
+    // st.push(111);
+    // st.pop();
+   
+    //     if(st.empty()  == false)
+    // {
+    //     cout<<st.top()<<endl;
+    // }
+    // cout << st.top() << endl;
+
+    int  n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+    while (!st.empty()) // st.empty() == false
+    {
+      cout<<st.top()<<endl;
+      st.pop();
+    }
     
- myStack st;
+    
+
     return 0;
 }
